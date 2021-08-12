9545f003d87689cf3592768970bb6117c7b79811
NathanDavisBarrett <ndb.nathan.barrett@gmail.com>
2021-08-12 09:32:54 -0600

    Added Example Plugin Files

---
+++ build/example_plugin/CMakeFiles/_example_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_example_plugin.dir/ExampleUpdater.cc.o"
+  "CMakeFiles/_example_plugin.dir/module.cc.o"
+  "_example_plugin.cpython-39-x86_64-linux-gnu.so"
+  "_example_plugin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_example_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
